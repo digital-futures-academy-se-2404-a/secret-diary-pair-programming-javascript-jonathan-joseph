b7e15762643c7fea63dc04980ccdd6c7fdbf7acc
jpmono416 <jpmono416@gmail.com>
2024-04-23 16:21:08 +0100

    Modified package.json. Included Diary class and tests for unlocking the diary

--- spec/Diary.spec.js
+++ spec/Diary.spec.js
@@ -1,4 +1,58 @@
-//* Diary should be locked by default
+import Diary from "../src/Diary.js";
 
-//* Diary should be unlocked with correct password
+describe('Diary', () => { 
 
+    it('should be locked by default', () => {
+        // Arrange
+        const diary = new Diary();
+
+        // Assert
+        expect(diary.isLocked()).toBeTrue();
+    });
+  
+    it('should be unlocked with correct password', () => {
+        // Arrange
+        const diary = new Diary(1234);
+
+        // Act
+        diary.unlock(1234);
+
+        // Assert
+        expect(diary.isLocked()).toBeFalse();
+    });
+  
+    it('should not be unlocked with incorrect password', () => {
+        // Arrange
+        const diary = new Diary(1234);
+
+        // Act
+        diary.unlock(4321);
+
+        // Assert
+        expect(diary.isLocked()).toBeTrue();
+    });
+
+    it('should not be unlocked with undefined password', () => {
+        // Arrange
+        const diary = new Diary(1234);
+
+        // Act
+        diary.unlock();
+
+        // Assert
+        expect(diary.isLocked()).toBeTrue();
+    });
+  
+    it('should not be locked by entering pin again', () => {
+        // Arrange
+        const diary = new Diary(1234);
+
+        // Act
+        diary.unlock(1234);
+        diary.unlock(1234);
+
+        // Assert
+        expect(diary.isLocked()).toBeFalse();
+    });
+
+});
